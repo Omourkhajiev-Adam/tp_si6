a277459a6538b041f00d3403a6a39b991da34b2b
Omourkhajiev-Adam <adam.omourkhajiev@bts-malraux.net>
2015-01-21 12:03:22 +0000

    Premier truc

---
+++ main.c
@@ -0,0 +1,12 @@
+double operandeA, operandeB, resultat;
+
+cout <<" Entrer l'opérande A " <<endl;
+cin >> operandeA;
+
+cout <<" Entrer l'opérande B " <<endl;
+cin >> operandeB;
+
+resultat = operandeA * operandeB;
+resultat = operandeA / operandeB;
+
+
